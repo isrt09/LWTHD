63032db1fdcf3a02a9ef3d6967a8255d8ff38545
mazed <mazedur@gmail.com>
2021-01-06 01:57:17 +0600

    loop test

--- wp-content/themes/alpha/index.php
+++ wp-content/themes/alpha/index.php
@@ -1,3 +1,18 @@
-<?php 
-$title = get_bloginfo('name');
-echo change_case($title);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php bloginfo('title'); ?></title>
+</head>
+<body>
+	<?php 
+		if(have_posts()):
+			while(have_posts()):the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+		<?php endwhile; else :
+			echo "Not Found!";
+		endif;
+	?>	
+</body>
+</html>
