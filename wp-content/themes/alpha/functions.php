00fd83b429236563d5c5c8589d362e4ce961abcf
mazed <mazedur@gmail.com>
2021-01-06 02:24:17 +0600

    Theme Bootstrapping

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -1,6 +1,14 @@
 <?php 
-
-function change_case($text){
-	return strtoupper($text);
+function alpha_bootstrap(){
+	load_theme_textdomain('alpha');
+	add_theme_support('post-thumbnails');
+	add_theme_support('title-tag');
 }
-		
+
+add_action('after_setup_theme','alpha_bootstrap');
+
+function alpha_assets(){
+	wp_enqueue_style('alpha',get_stylesheet_uri());
+	wp_enqueue_style("bootstrap",'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
+}
+add_action('wp_enqueue_scripts','alpha_assets');
