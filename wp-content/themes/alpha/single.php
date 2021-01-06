1d81efac8f8405af5977d06ab0270211d75af76c
mazed <mazedur@gmail.com>
2021-01-06 22:50:10 +0600

    single post template

--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -24,39 +24,24 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <h2 class="post-title  text-center"><?php the_title(); ?></h2>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-4">
-                        <p>
-                            <strong><?php the_author() ?></strong><br/>
-                            <?php echo get_the_date('M d, Y'); ?>, <?php the_time('g:i A'); ?>
-                        </p>
-                        <?php echo get_the_tag_list("<ul class=\"list-unstyled\"><li>","</li><li>","</li></ul>"); ?>
+                    <div class="col-md-10 offset-md-1 text-center">
+                        <p><strong><?php the_author() ?></strong><br/>
+                            <?php echo get_the_date('M d, Y'); ?>, <?php the_time('g:i A'); ?>                            
+                        </p>                        
                     </div>
-                    <div class="col-md-8  text-justify">
-                        <p><?php the_post_thumbnail('large',['class'=>'img-fluid']);?></p>                       
-                           <?php 
-                                if(is_single()): the_content();
-                                    else : the_excerpt();
-                                endif;
-                            ?>                                                          
+                    <div class="col-md-10 offset-md-1 text-justify">
+                       <p><?php the_post_thumbnail('large',['class'=>'img-fluid']);?></p>
+                       <?php the_content() ?>
                     </div>
                 </div>
             </div>          
         </div>      
     <?php }
     ?>       
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-8">
-            <h2>Pagination should be fixing soon....</h2>
-            <?php the_posts_pagination(['']); ?>
-        </div>
-    </div>
 </div>
 <div class="footer">
     <div class="container">
