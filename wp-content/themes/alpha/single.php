2b48aa1df9b451b0bdad0456b110b35de919124a
mazed <mazedur@gmail.com>
2021-01-11 02:41:15 +0600

    Single post seperation multi-pages

--- wp-content/themes/alpha/single.php
+++ wp-content/themes/alpha/single.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <body <?php body_class();?>>
-<?php get_template_part('hero'); ?>
+<?php get_template_part('template-parts/common/hero'); ?>
 <div class="container">
     <div class="row">
         <div class="col-md-8">
-            <div class="posts">
+            <div class="posts">                
                 <?php 
                     while(have_posts()){  the_post(); ?>            
                     <div class="post" <?php post_class(); ?>>
@@ -28,6 +28,7 @@
                                             the_post_thumbnail('large',['class'=>'img-fluid']);
                                             echo "</a>";
                                             the_content();
+                                            wp_link_pages();
                                             next_post_link();
                                             previous_post_link();
                                         }
